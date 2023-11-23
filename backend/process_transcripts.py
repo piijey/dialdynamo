88ee9ac8ea4b6efe54bd6d4cdfb13545672bd9c5
piijey <pj.223f@gmail.com>
2023-11-24 00:32:03 +0900

    identify newly added parts from the transcript, and add timeout

--- backend/process_transcripts.py
+++ backend/process_transcripts.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 from backend.models import Message
-from typing import Optional
+from typing import Optional, Tuple
 
 TIME_DELTA = timedelta(milliseconds=500) #前回のメッセージとの時間差の閾値
 
-def process(current: Message, prev: Message, time_delta: Optional[timedelta] = TIME_DELTA) -> Message:
+def get_segment(current: Message, prev: Message, time_delta: timedelta) -> Message:
     """
     以前のリクエストから TIME_DELTA 以上経過していれば、確定とみなして直前のユーザ入力を返す
     current: 最新の transcript
@@ -18,16 +18,35 @@ def process(current: Message, prev: Message, time_delta: Optional[timedelta] = T
     return segment
 
 
+def process(current: Message, prev: Message, message_list: list, time_delta: Optional[timedelta] = TIME_DELTA) -> Tuple[Message, list]:
+    """
+    current: transcript (最新)
+    prev: transcript (直前)
+    segment: user ユーザ入力の最新の確定部分
+    """
+    segment = get_segment(current, prev, time_delta) #最新の確定した transcript
+    if segment.text is None:
+        # segment は Messageクラスの初期値のまま、message_list は変更なし
+        pass
+    else:
+        # segment から、新たに追加された文字列を抜きだす
+        for m in message_list:
+            segment.text = segment.text.replace(m.text, "").strip()
+        message_list.append(segment)
+    return segment, message_list
+
+
 def test():
     import json
     testfile = "dialdynamo_note/231118_text_frontend.txt"
     prev = Message()
+    messages = []
     with open(testfile, "r") as f:
         for line in f:
             timestamp, text = line.strip().split("\t")
             current = Message(role="transcript", text=text, timestamp=timestamp)
             if prev.timestamp:
-                segment = process(current, prev)
+                segment, messages = process(current, prev, messages, timedelta(milliseconds=10))
                 if segment.text:
                     print(segment.text)
             prev = current
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import asyncio
 import json
+from datetime import timedelta, datetime, timezone
 from backend.models import Message
 from backend.process_transcripts import process
 
@@ -19,27 +21,31 @@ app.add_middleware(
 )
 
 # 設定
-from datetime import timedelta
-TIME_DELTA = timedelta(milliseconds=500) #前回のメッセージとの時間差の閾値
+TIMEOUT = 0.5 #(秒)
+TIME_DELTA = timedelta(seconds=TIMEOUT) #前回のメッセージとの時間差の閾値
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    prev_transcript = Message()
-    user_message = Message()
+    prev_transcript = Message() #直前の入力内容
+    messages = [] #過去の入力内容
     try:
         await websocket.accept()
         while True:
+            user_message = Message()
             try:
-                data = await websocket.receive_text()
+                data = await asyncio.wait_for(websocket.receive_text(), timeout=TIMEOUT)
                 transcript = Message(role="transcript", **json.loads(data))
                 print(f"transcript:\t{transcript.timestamp} {transcript.text}")
-                user_message = process(transcript, prev_transcript, TIME_DELTA)
-                if user_message.text:
-                    print(f"user_message:\t{user_message.timestamp} {user_message.text}")
-                    await websocket.send_text(user_message.model_dump_json())
+                user_message, messages = process(transcript, prev_transcript, messages, TIME_DELTA)
                 prev_transcript = transcript
+            except asyncio.TimeoutError: #TIMEOUT秒間ユーザ入力がないとき、ユーザ入力を確定する
+                timeout_message = Message(role="timeout", timestamp=datetime.now(timezone.utc))
+                user_message, messages = process(timeout_message, prev_transcript, messages, TIME_DELTA)
             except json.JSONDecodeError as e:
                 print(f"server.py JSONのデコードに失敗: {e}")
+            if user_message.text:
+                print(f"user_message:\t{user_message.timestamp} {user_message.text}")
+                await websocket.send_text(user_message.model_dump_json())
     except WebSocketDisconnect as e:
         print(f"server.py Websocketの接続に失敗: {e}")
 
